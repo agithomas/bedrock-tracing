da67b90fcc4d2354ee42e532e14a07be82b1138a
Agi K Thomas <agi.thomas@elastic.co>
2025-02-26 23:48:45 +0530

    Added readme contents

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bedrock_tracing",
-    version="0.2.2",
+    version="0.2.4",
     author="Agi K Thomas",
     author_email="agikthomas@hotmail.com",
     description="A Python library for tracing Bedrock agent responses with OpenTelemetry.",
